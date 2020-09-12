7fc4cd221285f2db0c930d4c9dfb0eda3c48f7e6
zeez <zeezrdq@gmail.com>
2020-09-12 18:07:36 +0200

    created a mongoDB connection

--- server.js
+++ server.js
@@ -1,6 +1,20 @@
 const express = require("express");
-
+const connectDB = require("./config/db");
 const app = express();
+
+//  Connect Database
+connectDB();
+
+// Init Middleware
+app.use(
+  express.json({
+    extended: false,
+  })
+);
+
+app.get("/", (req, res) => {
+  res.json({ msg: "welcome to our website" });
+});
 
 // Define Routes
 
