5d2ea78c2e3b2e33320fc2acd0c72a5688570429
zeez <zeezrdq@gmail.com>
2020-09-12 23:51:45 +0200

    added routing and initated some pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,27 @@
-import React from "react";
-
+import React, { Fragment } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import "./App.css";
+import Login from "./components/auth/Login";
+import Register from "./components/auth/Register";
+import Navbar from "./components/layouts/Navbar";
+import About from "./components/pages/About";
+import Home from "./components/pages/Home";
 
 const App = () => {
   return (
-    <div className="App">
-      <h1>Guestbook</h1>
-    </div>
+    <Router>
+      <Fragment>
+        <Navbar />
+        <div className="container">
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route exact path="/about" component={About} />
+            <Route exact path="/register" component={Register} />
+            <Route exact path="/login" component={Login} />
+          </Switch>
+        </div>
+      </Fragment>
+    </Router>
   );
 };
 
