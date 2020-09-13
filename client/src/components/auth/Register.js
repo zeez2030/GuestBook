ecd457e42aef49d1543e6e382d85c067c6a3ae07
zeez <zeezrdq@gmail.com>
2020-09-13 02:57:10 +0200

    created a funcional register view that registers a user TODO alerts context

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,9 +1,84 @@
-import React from "react";
+import React, { useContext, useState } from "react";
+import AuthContext from "../../context/auth/authContext";
 
 const Register = () => {
+  const authContext = useContext(AuthContext);
+  const { register, isAuthenticated } = authContext;
+
+  const [user, setUser] = useState({
+    name: "",
+    email: "",
+    password: "",
+    password2: "",
+  });
+
+  const { name, email, password, password2 } = user;
+
+  const onChange = (e) => setUser({ ...user, [e.target.name]: e.target.value });
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    register({
+      name,
+      email,
+      password,
+    });
+  };
+
   return (
-    <div>
-      <h1>Register page</h1>
+    <div className="form-container">
+      <h1>
+        Account <span className="text-primary"> Register</span>
+      </h1>
+      <form onSubmit={onSubmit}>
+        <div className="form-group">
+          <label htmlFor="name">Name</label>
+          <input
+            type="text"
+            name="name"
+            value={name}
+            onChange={onChange}
+            required
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="email">Email</label>
+          <input
+            type="email"
+            name="email"
+            value={email}
+            onChange={onChange}
+            required
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="password">Password</label>
+          <input
+            type="password"
+            name="password"
+            value={password}
+            onChange={onChange}
+            required
+            minLength="6"
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="password2">Confirm Password</label>
+          <input
+            type="password"
+            name="password2"
+            value={password2}
+            onChange={onChange}
+            required
+            minLength="6"
+          />
+        </div>
+        <input
+          type="submit"
+          value="Register"
+          className="btn btn-primary btn-block"
+        />
+      </form>
     </div>
   );
 };
