a9d46a8e20f440db7238885b9832a07fe99f826c
zeez <zeezrdq@gmail.com>
2020-09-13 03:51:31 +0200

    added a private route for the homepage

--- client/src/App.js
+++ client/src/App.js
@@ -7,6 +7,7 @@ import Alerts from "./components/layouts/Alerts";
 import Navbar from "./components/layouts/Navbar";
 import About from "./components/pages/About";
 import Home from "./components/pages/Home";
+import PrivateRoute from "./components/routing/PrivateRoute";
 import AlertState from "./context/alert/AlertState";
 import AuthState from "./context/auth/AuthState";
 import MessageState from "./context/messages/MessageState";
@@ -27,7 +28,7 @@ const App = () => {
               <div className="container">
                 <Alerts />
                 <Switch>
-                  <Route exact path="/" component={Home} />
+                  <PrivateRoute exact path="/" component={Home} />
                   <Route exact path="/about" component={About} />
                   <Route exact path="/register" component={Register} />
                   <Route exact path="/login" component={Login} />
